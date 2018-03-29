a21e6b3aa5f751b10ed4362455060556d5592169
zhangya <zhangya@mail.ustc.edu.cn>
2018-03-29 17:18:09 +0800

    support specify kernel vector

--- CreateKernelListWithVariable.m
+++ CreateKernelListWithVariable.m
@@ -1,8 +1,12 @@
 function [kernelcellaux,kerneloptioncellaux,variablecellaux]=CreateKernelListWithVariable(variablecell,dim,kernelcell,kerneloptioncell)
-
-
 j=1;
 for i=1:length(variablecell)
+    if ~ischar(variablecell{i})
+        variablecellaux{j} = variablecell{i};
+        kernelcellaux{j} = kernelcell{i};
+        kerneloptioncellaux{j} = kerneloptioncell{i};
+        continue;
+    end
     switch variablecell{i}
         case 'all'
             kernelcellaux{j}=kernelcell{i};
@@ -11,17 +15,17 @@ for i=1:length(variablecell)
             j=j+1;    
         case 'single'
             for k=1:dim
-            kernelcellaux{j}=kernelcell{i};
+                kernelcellaux{j}=kernelcell{i};
+                kerneloptioncellaux{j}=kerneloptioncell{i};
+                variablecellaux{j}=k;
+                j=j+1;
+            end    
+	    case 'random'
+	    	kernelcellaux{j}=kernelcell{i};
             kerneloptioncellaux{j}=kerneloptioncell{i};
-            variablecellaux{j}=k;
+	    	indicerand=randperm(dim);
+	    	nbvarrand=floor(rand*dim)+1;         
+   	    	variablecellaux{j}=indicerand(1:nbvarrand);
             j=j+1;
-            end;    
-	case 'random'
-		kernelcellaux{j}=kernelcell{i};
-        kerneloptioncellaux{j}=kerneloptioncell{i};
-		indicerand=randperm(dim);
-		nbvarrand=floor(rand*dim)+1;         
-   		variablecellaux{j}=indicerand(1:nbvarrand);
-        j=j+1;
-        end;
+    end
 end;
